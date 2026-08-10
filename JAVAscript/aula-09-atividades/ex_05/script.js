7d256ecfc05b33d313736c163a43de819d8a3d62
EDUARDO STEINBACH <eduardo_steinbach151@estudante.sesisenai.org.br>
2026-08-10 17:11:19 -0300

    .

---
+++ JAVAscript/aula-09-atividades/ex_05/script.js
@@ -0,0 +1,17 @@
+const h2 = document.querySelector("h2")
+const btn = document.querySelector("button")
+const analise = document.querySelector("#analise")
+
+btn.addEventListener("click", function(){
+    let primeiraDiv = document.createElement("div")
+    primeiraDiv.appendChild(primeiraDiv)
+    analise.appendChild(primeiraDiv)
+
+    let segundaDiv = document.createElement("div")
+    segundaDiv.textContent = h2.innerHTML
+    analise.appendChild(segundaDiv)
+
+    let terceiraDiv = document.creatElement("div")
+    terceiraDiv.textContent = h2.style.color;
+    analise.appendChild(terceiraDiv)
+})
---
+++ JAVAscript/aula-09-atividades/ex_06/script.js
@@ -0,0 +1,6 @@
+const h1 = document.querySelector("h1")
+const btn = document.querySelector("button")
+
+btn.addEventListener("click",function(){
+    h1.textContent =  "Título Modificado com Sucesso!"
+})
